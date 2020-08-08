ff7e89d0da22fee3c39d7d8e6247fcfc00a2dce8
Dmytro Struk <dmytro.struk1@gmail.com>
2020-08-09 01:21:44 +0300

    7. Reverse Integer

---
+++ easy-level/7. reverse-integer.py
@@ -0,0 +1,9 @@
+import math
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = int(str(abs(x))[::-1])
+        if math.pow(2, 31) <= res:
+            return 0
+
+        return res if x > 0 else res - res * 2
