bce61c7fb37ce4d0689e02dcad186af536274e41
Dmytro Struk <dmytro.struk1@gmail.com>
2020-08-09 13:29:34 +0300

    27. Remove Element

---
+++ easy-level/27. remove-element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+                i -= 1
+
+            i += 1
+        
+        return len(nums)
